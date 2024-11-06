dbdb6853284476a503236e136e418d0a3c6642e4
Dennys Fredericci <dennys@fredericci.com.br>
2024-11-06 14:02:46 +0100

    Add Daisy UI Components

--- src/main/webui/tailwind.config.js
+++ src/main/webui/tailwind.config.js
@@ -4,11 +4,19 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem'
+      },
+    },
     extend: {},
   },
   plugins: [
     require("@tailwindcss/typography"),
     require("daisyui")
   ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  }
 }
 
